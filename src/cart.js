91f72f3dff93ecefe8485d0320abbcfa9e3bb1bd
Guadeloupe33 <stevenfrancius@gmail.com>
2025-04-15 14:30:13 -0400

    added the checkout and clear cart funationality

--- src/cart.js
+++ src/cart.js
@@ -84,3 +84,46 @@ let generateCartItems = () => {
     generateCartItems();
     localStorage.setItem("data", JSON.stringify(basket));
   };
+  let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    TotalAmount();
+  };
+  
+  let removeItem = (id) => {
+    let selectedItem = id;
+    // console.log(selectedItem.id);
+    basket = basket.filter((x) => x.id !== selectedItem.id);
+    generateCartItems();
+    TotalAmount();
+    localStorage.setItem("data", JSON.stringify(basket));
+  };
+  
+  let clearCart = () => {
+    basket = [];
+    generateCartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+  };
+  
+  let TotalAmount = () => {
+    if (basket.length !== 0) {
+      let amount = basket
+        .map((x) => {
+          let { item, id } = x;
+          let search = shopItemsData.find((y) => y.id === id) || [];
+  
+          return item * search.price;
+        })
+        .reduce((x, y) => x + y, 0);
+      // console.log(amount);
+      label.innerHTML = `
+      <h2>Total Bill : $ ${amount}</h2>
+      <button class="checkout">Checkout</button>
+      <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+      `;
+    } else return;
+  };
+  
+  TotalAmount();
